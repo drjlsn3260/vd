c8dcaae85501e717ea9b4a45f403e3589c03e855
thofoc3691 <58833107+thofoc3691@users.noreply.github.com>
2020-09-23 23:45:37 +0800

    Update script.js

--- qr/qr1/script.js
+++ qr/qr1/script.js
@@ -67,7 +67,7 @@ function init() {
   add.className = 'qr-add';
   add.textContent = '添加二维码生成器';
   add.addEventListener('click', () => {
-    content.insertBefore(createQR(text), add)
+    content.insertBefore(createQR('https://is.gd/n8888'), add)
   });
   let share = document.querySelector('button.qr-share');
   share.addEventListener('click', (e) => {
@@ -79,7 +79,7 @@ function init() {
     setTimeout(() => e.target.textContent = origin, 3000);
   });
   content.appendChild(add);
-  let localQr = JSON.parse(localStorage.getItem('_local_qr_codes_') || text);
+  let localQr = JSON.parse(localStorage.getItem('_local_qr_codes_') || '["https://is.gd/n8888"]');
   let queryQr = parseQrFromQuery();
   localQr.forEach(chl => content.insertBefore(createQR(chl), add));
   if (queryQr) {
